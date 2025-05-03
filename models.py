d41ca9ba0a35d2eb7bb8b42e2a3ddf31f1b0946c
Олег Бовкуш <97238214+obovkush@users.noreply.github.com>
2025-05-03 21:14:36 +1000

    Add models file

---
+++ models.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class ChatHistory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_message = db.Column(db.Text, nullable=False)
+    ai_response = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_message': self.user_message,
+            'ai_response': self.ai_response,
+            'timestamp': self.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+        }
